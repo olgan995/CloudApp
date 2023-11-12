ffb8acb6f34cdbdeced97f516af192e4a7b38bc7
olgan <olga.nesterova@haw-hamburg.de>
2023-11-12 14:31:39 +0100

    refactired TaskController

--- backend/src/main/java/com/haw/task/controller/impl/TaskController.java
+++ backend/src/main/java/com/haw/task/controller/impl/TaskController.java
@@ -1,20 +1,16 @@
 package com.haw.task.controller.impl;
 
 import com.haw.appuser.common.exceptions.UserNotFoundByUsernameException;
-import com.haw.appuser.dataaccess.api.entity.AppUser;
 import com.haw.appuser.dataaccess.api.repo.UserRepository;
 import com.haw.appuser.logic.api.UserService;
 import com.haw.task.common.TaskDto;
 import com.haw.task.common.TaskNotFoundException;
+import com.haw.task.common.ForbiddenOperationException;
 import com.haw.task.dataaccess.api.entity.Task;
 import com.haw.task.dataaccess.api.repo.TaskRepository;
 import com.haw.task.logic.impl.TaskServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -36,10 +32,14 @@ public class TaskController {
 
 
     @GetMapping
-    //@PreAuthorize("#userId == authentication.name")
-    //public ResponseEntity<List<Task>> getTasks(@AuthenticationPrincipal String userId) {
     public List<Task> getTasksForCurrentUser() throws UserNotFoundByUsernameException {
         return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long taskId) throws UserNotFoundByUsernameException {
+        Task task = taskService.getTaskById(taskId);
+        return ResponseEntity.ok(task);
     }
 
     @PostMapping
@@ -49,9 +49,41 @@ public class TaskController {
         return ResponseEntity.status(201).body(newTask);
     }
 
-    @PatchMapping
-    public ResponseEntity<Task> updateTask(@RequestBody TaskDto task, Long taskId) throws UserNotFoundByUsernameException, TaskNotFoundException {
+    @PatchMapping("/{taskId}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long taskId, @RequestBody TaskDto task)
+            throws UserNotFoundByUsernameException, TaskNotFoundException, ForbiddenOperationException {
+
         Task newTask = taskService.updateTask(taskId, task);
         return ResponseEntity.status(200).body(newTask);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity<String> deleteTask(@PathVariable Long taskId) throws TaskNotFoundException, UserNotFoundByUsernameException, ForbiddenOperationException {
+
+        try {
+            taskService.deleteTask(taskId);
+        } catch (TaskNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (UserNotFoundByUsernameException e) {
+            throw new RuntimeException(e);
+        } catch (ForbiddenOperationException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok("Task with ID " + taskId + " has been deleted");
+    }
+
+    @ExceptionHandler(ForbiddenOperationException.class)
+    private ResponseEntity<String> handleUnauthorizedOperationException(ForbiddenOperationException ex) {
+        int statusCode = ex.getStatusCode();
+        String errorMessage = ex.getMessage();
+        return ResponseEntity.status(statusCode).body(errorMessage);
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    private ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+        int statusCode = ex.getStatusCode();
+        String errorMessage = ex.getMessage();
+        return ResponseEntity.status(statusCode).body(errorMessage);
     }
 }
