d0ac683dafbfd61e93cca9f7f81571a184e6dafe
Neophyte <119861284+blackneophyte@users.noreply.github.com>
2023-11-13 17:28:51 +0100

    Save authentication token in localStorage on login and the user's authentication status is checked by verifying the presence of the token. (#21)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Auth from "./Auth"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Auth from "./Auth";
 import Layout from "./components/layout/Layout";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Logout from "./components/auth/Logout";
 import Test from "./components/tasks/Test";
 
+const isAuthenticated = () => {
+    const token = localStorage.getItem('token');
+    console.log("token: ", token)
+    return !!token;
+};
+
 function App() {
+    const authenticated = isAuthenticated();
+    console.log("Is authenticated:", authenticated);
+
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/auth" element={<Layout> <Auth /> </Layout>} />
-            <Route path="/" element={<Layout> <Login /> </Layout>} />
-            <Route path="/login" element={<Layout> <Login /> </Layout>} />
-            <Route path="/register" element={<Layout> <Register /> </Layout>} />
-            <Route path="/logout" element={<Layout> <Logout /> </Layout>} />
-            <Route path="/tasks" element={<Layout> <Test /> </Layout>} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/tasks" element={authenticated ? <Layout> <Test /> </Layout>  : <Navigate to="/login"/>}/>
         </Routes>
       </BrowserRouter>
-  )
+  );
 }
 
 export default App
