407817fc6cc7aeaef1c54383ec988656c0c69385
olgan <olga.nesterova@haw-hamburg.de>
2023-11-19 14:24:36 +0100

    added @EnableScheduling annotation

--- backend/src/main/java/com/haw/Application.java
+++ backend/src/main/java/com/haw/Application.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -26,6 +27,7 @@ import java.util.List;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
