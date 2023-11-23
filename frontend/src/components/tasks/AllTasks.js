33a339b1139ab723d3d9b059686621b72767da37
Ivoni Laoli <ivoni.laoli@hansecom.com>
2023-11-23 18:23:48 +0100

    8 - implemented AddTask

--- frontend/src/components/tasks/AllTasks.js
+++ frontend/src/components/tasks/AllTasks.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Card, ListGroup } from 'react-bootstrap';
+import {Button, Container} from 'react-bootstrap';
 import api from "../../services/api";
 import { setAuthToken } from "../../services/api";
 import Task from './Task';
+import AddTask from "./AddTask";
 
 const AllTasks = () => {
     const [tasks, setTasks] = useState([]);
@@ -26,19 +27,52 @@ const AllTasks = () => {
         fetchTasks();
     }, []);
 
+    const [showModal, setShowModal] = useState(false);
+    const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
+    const handleAddTask = async (formData) => {
+        try {
+            const response = await api.createTask(formData);
+            console.log('Task created:', response.data);
+
+            handleClose();
+
+            // Fetch tasks again after adding a new task
+            const updatedTasks = await api.getAllTasks();
+            setTasks(updatedTasks.data);
+
+        } catch (error) {
+            setError('Error adding task. Please try again.');
+            console.error('Task add error:', error);
+        }
+    };
+
+
     return (
         <Container className="mt-4">
+
             <h1 className="text-center all-tasks-title">Todo List</h1>
             {error && <p style={{ color: 'red' }}>{error}</p>}
-            <Card>
-                <Card.Body>
-                    <ListGroup variant="flush">
-                        {tasks.map((task) => (
-                            <Task key={task.id} task={task} />
-                        ))}
-                    </ListGroup>
-                </Card.Body>
-            </Card>
+
+            <div className="d-flex justify-content-center m-3">
+                <Button variant="primary" className="rounded-pill" onClick={handleShow}>
+                    Add Task
+                </Button>
+
+                {/* Render the AddTask component */}
+                <AddTask
+                    showModal={showModal}
+                    handleClose={handleClose}
+                    handleAddTask={handleAddTask}
+                />
+            </div>
+
+            <div className="d-flex m-3">
+                {tasks.map((task) => (
+                    <Task key={task.id} task={task} />
+                ))}
+            </div>
+
         </Container>
     );
 };
--- frontend/src/components/tasks/Task.js
+++ frontend/src/components/tasks/Task.js
@@ -3,20 +3,42 @@ import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Task = ({ task }) => {
+
+    const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+
     return (
-        <Card style={{ width: '18rem', margin: '10px', backgroundColor: task.completed ? '#32CD32' : '#EA7B7B' }}>
-            <Card.Body>
-                <Card.Title>{task.taskName}</Card.Title>
-                <Card.Text>
-                    Description: {task.description}
-                    <br />
-                    Due Date: {task.dueDate}
-                    <br />
-                    Status: {task.completed ? 'Completed' : 'Incomplete'}
-                </Card.Text>
-                <Link to={{ pathname: `/update-task/${task.id}`, state: { task } }}>
-                    <Button variant="primary">Task ändern</Button>
-                </Link>
+        <Card className="m-2" style={{ width: '18rem' }}>
+            <Card.Body
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    backgroundColor: task.completed ? '#90EE90' : '#DCDCDC',
+                }}
+            >
+                <div  className="mb-2">
+                    <Card.Title>{task.taskName || 'N/A'}</Card.Title>
+                    <Card.Text>{task.description}</Card.Text>
+                    <Card.Text>Due Date: {new Date(task.dueDate).toLocaleString('de-DE', options)}</Card.Text>
+                    <Card.Text>Status: {task.completed ? 'Completed' : 'Incomplete'}</Card.Text>
+                </div>
+
+                <div className="d-flex justify-content-between mt-auto">
+                    <Link to={{ pathname: `/update-task/${task.id}`, state: { task } }}>
+                        <Button
+                            className="rounded-pill w-45"
+                            size="sm"
+                            variant="primary"
+                        >Update Task</Button>
+                    </Link>
+                </div>
+
             </Card.Body>
         </Card>
     );
